727f82b0bb9e9de8155be64296aef6d6a1c675c6
fablnt <fabio.lentini@mail.polimi.it>
2024-10-10 16:31:07 +0000

    problem methods implementation

--- includes/core.hpp
+++ includes/core.hpp
@@ -13,22 +13,30 @@ public:
     {
     };
 
-    IcoNS(const unsigned int nx, const unsigned int ny, const unsigned int nz, const double dt, const double T, const double Re, const std::string &input_file, const std::string &output_file)
+    IcoNS(const double lx, const double ly, const double lz, const unsigned int nx, const unsigned int ny, const unsigned int nz, const double dt, const double T, const double Re, const std::string &input_file, const std::string &output_file)
         : grid(nx, ny, nz),
           dt(dt),
           T(T),
           Re(Re),
+          lx(lx),
+          ly(ly),
+          lz(lz),
           nx(nx),
           ny(ny),
           nz(nz),
           input_file(input_file),
           output_file(output_file)
     {
+        dx = lx / nx;
+        dy = ly / ny;
+        dz = lz / nz;
     }
 
     void preprocessing(string &input_file); // grid initialization.
 
     void solve(); // solve the problem saving the ouput.
+
+    std::vector<double> functionF(const std::vector<double> &u, const std::vector<double> &v, const std::vector<double> &w, size_t i, size_t j, size_t k); // compute the source term.
 
     void solve_time_step(); // solve a time step.
 
@@ -39,7 +47,9 @@ private:
     const double dt;               // time step.
     const double T;                // final time.
     const double Re;               // Reynolds number.
+    const unsigned int lx, ly, lz; // lengths of edges of the domain.
     const unsigned int nx, ny, nz; // number of cells in the x,y,z directions.
+    const double dx, dy, dz;       // cell sizes in the x,y,z directions.
     ExactSolution exact_solution;  // exact solution.
     std::string input_file;        // input file.
     std::string output_file;       // output file.
---
+++ src/core.cpp
@@ -0,0 +1,125 @@
+#include "core.hpp"
+
+void IcoNS::preprocessing(string &input_file)
+{
+    // read the input file.
+    for (size_t i = 1; i < nx - 1; i++)
+    {
+        for (size_t j = 1; j < ny - 1; j++)
+        {
+            for (size_t k = 1; k < nz - 1; k++)
+            {
+                grid.u[i * ny * nz + j * nz + k] = 0.0;
+                grid.v[i * ny * nz + j * nz + k] = 0.0;
+                grid.w[i * ny * nz + j * nz + k] = 0.0;
+            }
+        }
+    }
+    for (size_t i = 0; i < nx; i++)
+    {
+        for (size_t j = 0; j < ny; j++)
+        {
+            for (size_t k = 0; k < nz; k++)
+            {
+                if (i == 0 || i == nx || j == 0 || j == ny || k == 0 || k == nz)
+                {
+                    grid.u[i * ny * nz + j * nz + k] = 1.0;
+                    grid.v[i * ny * nz + j * nz + k] = 1.0;
+                    grid.w[i * ny * nz + j * nz + k] = 1.0;
+                }
+            }
+        }
+        // pressure initialization.
+    }
+}
+
+void IcoNS::solve()
+{
+    double time = 0.0;
+
+    while (time < T)
+    {
+        solve_time_step();
+        time += dt;
+
+        output();
+    }
+}
+
+std::vector<double> IcoNS::functionF(const std::vector<double> &u, const std::vector<double> &v,
+                                     const std::vector<double> &w, size_t i, size_t j, size_t k)
+{
+    std::vector<double> f(3);
+    size_t l = i * ny * nz + j * nz + k;
+    f[0] = -(u[l] * (u[l + ny * nz] - u[l - ny * nz]) / (2 * dx) +
+             (v[l + nz] + v[l - nz] + v[l + ny * nz + nz] + v[l + ny * nz - nz]) / 4.0 *
+                 (u[l + ny * nz + nz] - u[l + ny * nz - nz]) / (2 * dy) +
+             (w[l + 1] + w[l - 1] + w[l + ny * nz + 1] + w[l + ny * nz - 1]) / 4.0 *
+                 (u[l + ny * nz + 1] - u[l + ny * nz - 1]) / (2 * dz)) +
+           1 / Re * ((u[l + ny * nz] - 2 * u[l] + u[l - ny * nz]) / (dx * dx) + (u[l + nz] - 2 * u[l] + u[l - nz]) / (dy * dy) + (u[l + 1] - 2 * u[l] + u[l - 1]) / (dz * dz));
+
+    f[1] = -();
+
+    return f;
+}
+
+void IcoNS::solve_time_step()
+{
+    std::vector<double> f(3);
+    std::vector<double> f_y2(3);
+    std::vector<double> f_y3(3);
+    std::vector<double> Y2_x(nx * ny * nz);
+    std::vector<double> Y2_y(nx * ny * nz);
+    std::vector<double> Y2_z(nx * ny * nz);
+    std::vector<double> Y3_x(nx * ny * nz);
+    std::vector<double> Y3_y(nx * ny * nz);
+    std::vector<double> Y3_z(nx * ny * nz);
+
+    for (size_t i = 1; i < nx - 1; i++)
+    {
+        for (size_t j = 1; j < ny - 1; j++)
+        {
+            for (size_t k = 1; k < nz - 1; k++)
+            {
+                f = functionF(grid.u, grid.v, grid.w, i, j, k);
+                // solve the momentum equations -> TODO later also with pressure
+                Y2_x[i * ny * nz + j * nz + k] = grid.u[i * ny * nz + j * nz + k] +
+                                                 64 / 120 * dt * f[0];
+                Y2_y[i * ny * nz + j * nz + k] = grid.v[i * ny * nz + j * nz + k] +
+                                                 64 / 120 * dt * f[1];
+                Y2_z[i * ny * nz + j * nz + k] = grid.w[i * ny * nz + j * nz + k] +
+                                                 64 / 120 * dt * f[2];
+
+                f_y2 = functionF(Y2_x, Y2_y, Y2_z, i, j, k);
+
+                Y3_x[i * ny * nz + j * nz + k] = Y2_x[i * ny * nz + j * nz + k] +
+                                                 50 / 120 * dt * f_y2[0] -
+                                                 34 / 120 * dt * f[0];
+                Y3_y[i * ny * nz + j * nz + k] = Y2_y[i * ny * nz + j * nz + k] +
+                                                 50 / 120 * dt * f_y2[1] -
+                                                 34 / 120 * dt * f[1];
+                Y3_z[i * ny * nz + j * nz + k] = Y2_z[i * ny * nz + j * nz + k] +
+                                                 50 / 120 * dt * f_y2[2] -
+                                                 34 / 120 * dt * f[2];
+
+                // update the grid values.
+                f_y3 = functionF(Y3_x, Y3_y, Y3_z, i, j, k);
+
+                grid.u[i * ny * nz + j * nz + k] = Y3_x[i * ny * nz + j * nz + k] +
+                                                   90 / 120 * dt * f_y3[0] -
+                                                   50 / 120 * dt * f_y2[0];
+                grid.v[i * ny * nz + j * nz + k] = Y3_y[i * ny * nz + j * nz + k] +
+                                                   90 / 120 * dt * f_y3[1] -
+                                                   50 / 120 * dt * f_y2[1];
+                grid.w[i * ny * nz + j * nz + k] = Y3_z[i * ny * nz + j * nz + k] +
+                                                   90 / 120 * dt * f_y3[2] -
+                                                   50 / 120 * dt * f_y2[2];
+            }
+        }
+    }
+}
+
+void IcoNS::output()
+{
+    // write the output file.
+}
