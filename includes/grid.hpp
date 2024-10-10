69478638584df7b8e546be750e5f7f4347c6982b
fablnt <fabio.lentini@mail.polimi.it>
2024-10-10 14:33:54 +0000

    grid class definition

--- includes/grid.hpp
+++ includes/grid.hpp
@@ -1,1 +1,22 @@
 // class representing the grid of the domain.
+
+#ifndef GRID_HPP
+#define GRID_HPP
+
+#include <iostream>
+#include <vector>
+
+struct Grid
+{
+    int nx, ny, nz;              // number of cells in the x,y,z directions.
+    std::vector<double> u, v, w; // velocity values of the grid cells.
+    std::vector<double> p;       // pressure values of the grid cells.
+
+    Grid(int nx, int ny, int nz) : nx(nx), ny(ny), nz(nz)
+    {
+        u.resize(nx * ny * nz);
+        v.resize(nx * ny * nz);
+        w.resize(nx * ny * nz);
+        p.resize((nx + 1) * (ny + 1) * (nz + 1));
+    }
+};
