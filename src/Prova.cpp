2151a3ff7cf53ce7dc7cd93c3ae0ab0ddf2815d7
Fozyhh <dandrea0105@gmail.com>
2024-10-21 18:58:00 +0200

    file handling

--- src/Prova.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-
-    std::cout << "Ciao";
-    return 0;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,18 @@
 
 int main()
 {
-    int main()
-    {
-        const double lx = 1000;
-        const double ly = 1000;
-        const double lz = 1000;
-        const unsigned int nx = 100;
-        const unsigned int ny = 100;
-        const unsigned int nz = 100;
-        const double dt = 1;
-        const double T = 5.0;
-        const double Re = 400.0;
-        IcoNS icoNS(lx, ly, lz, nx, ny, nz, dt, T, Re, "input.txt", "output.txt");
+    const double lx = 1000;
+    const double ly = 1000;
+    const double lz = 1000;
+    const unsigned int nx = 100;
+    const unsigned int ny = 100;
+    const unsigned int nz = 100;
+    const double dt = 1;
+    const double T = 5.0;
+    const double Re = 400.0;
+    IcoNS icoNS(lx, ly, lz, nx, ny, nz, dt, T, Re, "input.txt", "output.txt");
 
-        icoNS.solve();
+    icoNS.solve();
 
-        return 0;
-    }
+    return 0;
 }
