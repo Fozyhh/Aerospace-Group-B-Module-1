10c4790d97500d92a0e85dadb04c5d9897f1b42d
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2024-10-26 11:50:17 +0200

    Add some comments

--- src/boundary.cpp
+++ src/boundary.cpp
@@ -11,16 +11,22 @@ grid(grid_)
 ,prec(1/2)
 {}
 
+// The method, which takes as input only the time step, is called by the program at the start of the time step.
+// For each boundary node, it takes the exact value dor each component from the input and updates them.
+// It also updates those values that are not directly on a face, but need an approximation.
 void Boundary::update_boundary(double t){
 
+    // Each face is numbered from 0 to 5 and we treat every face separately
 
-    std::cout << "Check 0"<< std::endl;
-    size_t face=0; // left face
+
+    // LEFT FACE
+
+    size_t face=0;
 
     for (size_t k=0; k < nz; k++)
     {
-        grid->w[k] = boundary_value_w[face]->value(0,0,k,t); //0 0 0.5
-        grid->v[k] = boundary_value_v[face]->value(0,0,k,t); //0 0.5 0
+        grid->w[k] = boundary_value_w[face]->value(0,0,k,t);
+        grid->v[k] = boundary_value_v[face]->value(0,0,k,t);
     }
     grid->v[nz] = boundary_value_v[face]->value(0,0,nz,t);
 
@@ -44,8 +50,9 @@ void Boundary::update_boundary(double t){
     
 
 
-    std::cout << "Check 1"<< std::endl;
-    face=1; // right face
+    // RIGHT FACE
+
+    face=1;
 
     for (size_t k=0; k < nz; k++)
     {
@@ -74,8 +81,9 @@ void Boundary::update_boundary(double t){
 
 
 
-    std::cout << "Check 2"<< std::endl;
-    face=2; // front face
+    // FRONT FACE
+
+    face=2;
 
     for (size_t k=1; k < nz; k++)
     {
@@ -95,8 +103,9 @@ void Boundary::update_boundary(double t){
 
     
 
-    std::cout << "Check 3"<< std::endl;
-    face=3; //back face
+    // BACK FACE
+
+    face=3;
 
     for (size_t k=1; k < nz; k++)
     {
@@ -116,7 +125,9 @@ void Boundary::update_boundary(double t){
 
 
 
-    face=4; //lower face
+    // LOWER FACE
+
+    face=4;
 
     for (size_t j=0; j < ny+1; j++)
     {
@@ -138,7 +149,9 @@ void Boundary::update_boundary(double t){
 
 
 
-    face=5; //upper face
+    // UPPER FACE
+
+    face=5;
 
     for (size_t j=0; j < ny+1; j++)
     {
@@ -158,10 +171,11 @@ void Boundary::update_boundary(double t){
         grid->u[(ny+1) * (nz+1) * i + ny*(nz+1) + nz] = boundary_value_u[face]->value(i,ny,nz,t);
     }
 
+
 }   
 
 
-
+// Performs the approximation of the component u that isn't precisely on the boundary.
 double Boundary::approximate_boundary_u(size_t x, size_t y, size_t z, double t,size_t face) {
     double dv = ((boundary_value_v[face]->value(x, y + prec, z,t)) -
                 ((boundary_value_v[face]->value(x, y - prec, z,t)))) / (2*prec);
@@ -172,9 +186,11 @@ double Boundary::approximate_boundary_u(size_t x, size_t y, size_t z, double t,s
     return  boundary_value_u[face]->value(x, y, z, t) - (dv + dw) * (dx/2);
 }
 
+
+// Performs the approximation of the component v that isn't precisely on the boundary.
 double Boundary::approximate_boundary_v(size_t x, size_t y, size_t z, double t,size_t face) {
     double du = ((boundary_value_u[face]->value(x + prec, y, z,t)) -
-                ((boundary_value_u[face]->value(x- prec, y, z,t)))) / (2*prec);
+                ((boundary_value_u[face]->value(x - prec, y, z,t)))) / (2*prec);
     
     double dw =  ((boundary_value_w[face]->value(x, y, z + prec,t)) -
                  ((boundary_value_w[face]->value(x, y, z - prec,t)))) / (2*prec);
@@ -182,6 +198,8 @@ double Boundary::approximate_boundary_v(size_t x, size_t y, size_t z, double t,s
     return  boundary_value_v[face]->value(x, y, z, t) - (du + dw) * (dy/2);
 }
 
+
+// Performs the approximation of the component w that isn't precisely on the boundary.
 double Boundary::approximate_boundary_w(size_t x, size_t y, size_t z, double t,size_t face) {
     double du = ((boundary_value_u[face]->value(x + prec, y, z,t)) -
                 ((boundary_value_u[face]->value(x - prec, y, z,t)))) / (2*prec);
