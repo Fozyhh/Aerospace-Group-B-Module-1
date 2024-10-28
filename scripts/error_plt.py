5aa159bf8abbb29f3eef4013acbe13e5ed955c14
nncm-xbc <simon.hugot.67@gmail.com>
2024-10-28 09:46:32 +0000

    error plot improved

--- scripts/error_plt.py
+++ scripts/error_plt.py
@@ -6,9 +6,16 @@ time = data[:, 0]
 iter = data[:, 1]
 error = data[:, 2]
 
+ref = error[0] / (iter[1] ** 2)
+second_order = ref * (iter ** 2)
+
 plt.figure(figsize=(12, 6))
 #plt.plot(time, error, "r-", label="Error over time")
 plt.plot(iter, error, "g-", label="Error over iterations")
+plt.plot(iter, second_order, "b-", label="Second order")
+
+plt.yscale("log")
+plt.xscale("log")
 
 plt.xlabel("Time steps/Iterations")
 plt.ylabel("Error")
