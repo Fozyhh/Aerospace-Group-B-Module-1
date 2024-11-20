533952c27da7ef847c246a7098cb50d5e3f4590b
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2024-11-20 16:10:31 +0100

    Poisson solver creation

---
+++ includes/poissonSolver.hpp
@@ -0,0 +1,30 @@
+#ifndef POISSONSOLVER_HPP
+#define POISSONSOLVER_HPP
+
+#include "utils.hpp"
+
+class PoissonSolver
+{
+
+private:
+    const bool periodicX;
+    const bool periodicY;
+    const bool periodicZ;
+    std::array<Real, (NX+1)*(NY+1)*(NZ+1)> eigenvalues;
+
+
+public: 
+    PoissonSolver(const bool periodicX, const bool periodicY, const bool periodicZ)
+    : periodicX(periodicX),
+      periodicY(periodicY),
+      periodicZ(periodicZ)
+    {
+        // Here we will build the eigenvalues array -> Each eigenvalue is constructed considering the corresponding boolean of the direction
+    }
+
+    void solvePoisson(std::array<Real, (NX+1)*(NY+1)*(NZ+1)>& F);
+};
+
+#endif
+
+// We have to decide how to pass different types of boundaries (Periodic or Neumann). For now we will use a boolean
---
+++ src/poissonSolver.cpp
@@ -0,0 +1,7 @@
+#include "poissonSolver.hpp"
+
+void PoissonSolver::solvePoisson(std::array<Real, (NX+1)*(NY+1)*(NZ+1)>& F)
+{
+    
+}
+
