936f59e69988736990142b5dee02586ea8ce6531
Fozyhh <dandrea0105@gmail.com>
2024-11-26 13:34:47 +0100

    Communication with 2  processor at a time done

--- mpi.cpp
+++ mpi.cpp
@@ -7,7 +7,7 @@
 #define NY 4
 #define NZ 4
 
-#define PX 2
+#define PX 1
 #define PY 2
 #define PZ 1
 
@@ -99,8 +99,10 @@ int main(int argc, char **argv)
 
     if (NX % PX != 0 && coords[0] == dim_x - 1)
         dim_x++;
-    if ((NY + 1) % PY != 0 && coords[1] == dim_y - 1)
+    if ((NY + 1) % PY != 0 && coords[1] == 1/*dim_y - 1*/){
         dim_y++;
+    }
+        
 
     int res_x = 0;
     int res_y = 0;
@@ -123,71 +125,142 @@ int main(int argc, char **argv)
     int newDimY = dim_y + res_y;
     std::vector<int> grid_loc(newDimX * newDimY * dim_z);
 
-    // Probabilmente il problema è qui. Considerare che ogni rank definisce 
-    // la propria MPI_face (per il momento non è un problema perchè i processi che scambiano dati:
-    // 0 e 2, 1 e 3, sono adiacenti e quindi hanno la stessa dimensione di face)
-    MPI_Datatype MPI_face;
-    MPI_Type_vector(dim_y, dim_z, res_y, MPI_INT, &MPI_face);
-    MPI_Type_commit(&MPI_face);
+    
 
-    for (int i = 1; i < newDimX - 1; i++)
+
+    for (int i = 0; i < newDimX; i++)
+    {
+        for (int j = 0; j < newDimY ; j++)
+        {
+            for (int k = 0; k < dim_z; k++)
+            {
+
+                // ---> z   
+                // ^
+                // | y,  uscente x 
+                int glob_address_x = i +coords[0]*dim_x;
+                int glob_address_y =j+ coords[1]*(dim_y-2); // il -1 andrebbe solo nell'ultimo processore dato che ha la dim_y diversa dagli altri(o si fa una var diversa)
+                //boundary points segnati nella griglia
+                if(glob_address_x == 0 || glob_address_x==NX)
+                    grid_loc[i * newDimY * dim_z + j * dim_z + k] += 2;
+                if(glob_address_y== 0 || glob_address_y==NY)
+                    grid_loc[i * newDimY * dim_z + j * dim_z + k] += 3;
+                if(k==0 || k==NZ)
+                    grid_loc[i * newDimY * dim_z + j * dim_z + k] += 4;
+
+
+                //Setting ghost points to 0
+                if(rank == 0 && j == dim_y) grid_loc[i * newDimY * dim_z + j * dim_z + k] =0;
+                if(rank == 1 && j == 0) grid_loc[i * newDimY * dim_z + j * dim_z + k] =0;
+            }
+        }
+    }
+    
+    for (int i = 1; i < newDimX - 0; i++)
     {
         for (int j = 1; j < newDimY - 1; j++)
         {
             for (int k = 1; k < dim_z - 1; k++)
             {
                 grid_loc[i * newDimY * dim_z + j * dim_z + k] = 1;
+                
             }
         }
     }
 
-    MPI_Status status;
 
-    if (neighbors[0] != -2)
+    // Probabilmente il problema è qui. Considerare che ogni rank definisce 
+    // la propria MPI_face (per il momento non è un problema perchè i processi che scambiano dati:
+    // 0 e 2, 1 e 3, sono adiacenti e quindi hanno la stessa dimensione di face)
+    
+    //TODO: riguardare perchè testata con 2 proc e basta
+    MPI_Datatype MPI_face_x;
+    MPI_Type_vector(1,dim_z* dim_y, res_y, MPI_INT, &MPI_face_x);
+    MPI_Type_commit(&MPI_face_x);
+    // MPI_Status status;
+
+    // if (neighbors[0] != -2)
+    // {
+    //     MPI_Send(&grid_loc[newDimY * dim_z], 1, MPI_face_x, neighbors[0], rank, cart_comm);
+    // }
+
+    // if (neighbors[2] != -2)
+    // {
+    //     MPI_Recv(&grid_loc[(newDimX - 1) * newDimY * dim_z], 1, MPI_face_x, neighbors[2], neighbors[2], cart_comm, &status);
+    // }
+
+    //LO STRIDE INIZIA A CONTARE DALL INIZIO DEL BLOCCO PRECEDENTE!!!!
+    MPI_Datatype MPI_face_y;
+    MPI_Type_vector(dim_x,dim_z, (dim_y)*dim_z + dim_z, MPI_INT, &MPI_face_y);
+    MPI_Type_commit(&MPI_face_y);
+    MPI_Status status;
+    if (neighbors[1] != -2)
     {
-        MPI_Send(&grid_loc[newDimY * dim_z + dim_z + 1], 1, MPI_face, neighbors[0], rank, cart_comm);
+        MPI_Send(&grid_loc[dim_z], 1, MPI_face_y, neighbors[1], rank, cart_comm);
     }
 
-    if (neighbors[2] != -2)
+    if (neighbors[3] != -2)
     {
-        MPI_Recv(&grid_loc[(newDimX - 1) * newDimY * dim_z + dim_z + 1], 1, MPI_face, neighbors[2], neighbors[2], cart_comm, &status);
+        MPI_Recv(&grid_loc[dim_z*(dim_y)], 1, MPI_face_y, neighbors[3], neighbors[3], cart_comm, &status);
     }
 
     MPI_Barrier(cart_comm);
-
-    std::cout << " rank: " << rank /*rank << "N0: " << neighbors[0] << " N2: " <<neighbors[2]*/ << std::endl;
-    for (int i = 0; i < newDimX; i++)
-    {
-        for (int j = 0; j < newDimY; j++)
+    if (rank == 0){
+        std::cout << " rank: " << rank /*rank << "N0: " << neighbors[0] << " N2: " <<neighbors[2]*/ << std::endl;
+        for (int i = 0; i < newDimX; i++)
         {
-            for (int k = 0; k < dim_z; k++)
+            for (int j = 0; j < newDimY; j++)
             {
-                std::cout << grid_loc[i * newDimY * dim_z + j * dim_z + k] << " ";
+                for (int k = 0; k < dim_z; k++)
+                {
+                    std::cout << grid_loc[i * newDimY * dim_z + j * dim_z + k] << " ";
+                }
+                std::cout << std::endl;
             }
             std::cout << std::endl;
         }
-        std::cout << std::endl;
     }
 
     MPI_Barrier(cart_comm);
 
+    if (rank == 1){
+        std::cout << " rank: " << rank /*rank << "N0: " << neighbors[0] << " N2: " <<neighbors[2]*/ << std::endl;
+        for (int i = 0; i < newDimX; i++)
+        {
+            for (int j = 0; j < newDimY; j++)
+            {
+                for (int k = 0; k < dim_z; k++)
+                {
+                    std::cout << grid_loc[i * newDimY * dim_z + j * dim_z + k] << " ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    MPI_Barrier(cart_comm);
+
+    if (rank == 0)
+    {
+        std::cout << "Rank: " << rank << std::endl;
+        std::cout << "Dim_x: " << dim_x << " Dim_y: " << dim_y << std::endl;
+        std::cout << "Res_x: " << res_x << " Res_y: " << res_y << std::endl;
+        std::cout << "NewDimX: " << newDimX << " NewDimY: " << newDimY << std::endl;
+        std::cout << "upper neighbour: " << neighbors[1] << " lower: " << neighbors[3] << std::endl;
+        std::cout << "Coords: " << coords[0] << "," << coords[1] << std::endl<< std::endl;
+
+    }
+    MPI_Barrier(cart_comm);
     if (rank == 1)
     {
         std::cout << "Rank: " << rank << std::endl;
         std::cout << "Dim_x: " << dim_x << " Dim_y: " << dim_y << std::endl;
         std::cout << "Res_x: " << res_x << " Res_y: " << res_y << std::endl;
         std::cout << "NewDimX: " << newDimX << " NewDimY: " << newDimY << std::endl;
-
-    }
-
-    if (rank == 3)
-    {
-        std::cout << "Rank: " << rank << std::endl;
-        std::cout << "Dim_x: " << dim_x << " Dim_y: " << dim_y << std::endl;
-        std::cout << "Res_x: " << res_x << " Res_y: " << res_y << std::endl;
-        std::cout << "NewDimX: " << newDimX << " NewDimY: " << newDimY << std::endl;
-
-    }
+        std::cout << "upper neighbour: " << neighbors[1] << " lower: " << neighbors[3] << std::endl;
+        std::cout << "Coords: " << coords[0] << "," << coords[1] << std::endl;
+    }   
 
     MPI_Finalize();
     return 0;
