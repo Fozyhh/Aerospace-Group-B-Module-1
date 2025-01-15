306fcd19bb0c3cb87151c8ac282f895905744dd5
Fozyhh <dandrea0105@gmail.com>
2025-01-15 22:55:41 +0000

    fix f functions

--- src/newRK.cpp
+++ src/newRK.cpp
@@ -1,5 +1,5 @@
 #include "core.hpp"
-
+#include <fstream>
 
 void IcoNS::solve_time_step(Real time)
 {
@@ -92,8 +92,7 @@ void IcoNS::solve_time_step(Real time)
 
     //boundary.divergence(Y2_x, Y2_y, Y2_z, Y2_p, time + 64.0 / 120.0 * DT, 64.0);
     poissonSolver->solveNeumannPoisson(Y2_p);
-    
-
+    MPI_Barrier(cart_comm);
     // 2) y2_p pressure point exchange
     copyPressureToHalo(Y2_p,halo_p);
     MPI_Barrier(cart_comm);
@@ -147,33 +146,17 @@ void IcoNS::solve_time_step(Real time)
             }
         }
     }
+
     MPI_Barrier(cart_comm);
     exchangeData(Y2_x, newDimX_x, newDimY_x, dim_z, MPI_face_x_x, MPI_face_y_x,0,1);
     exchangeData(Y2_y, newDimX_y, newDimY_y, dim_z, MPI_face_x_y, MPI_face_y_y,1,0);
     exchangeData(Y2_z, newDimX_z, newDimY_z, dim_z_z, MPI_face_x_z, MPI_face_y_z,1,1);
-    // if(rank==0){
-
-    //         std::cout << "Y2_x: "<< std::endl;
-    //         for(int in = 0; in < newDimX_x; in++){
-    //             for (int j = 0; j < newDimY_x; j++){
-    //                 for (int k = 0; k < dim_z; k++)
-    //                 {
-    //                     std::cout << Y2_x[getx(in,j,k)] << " ";
-    //                 }
-    //                 std::cout << std::endl;
-    //             }
-    //             std::cout << std::endl;
-    //         }
-    // }
-    // int stop; std::cin >> stop;
     
     //3) Phi_p exchange 
     copyPressureToHalo(Phi_p,halo_phi);
     MPI_Barrier(cart_comm);
     exchangeData(halo_phi,(xSize[2] + 2), (xSize[1] + 2), xSize[0], MPI_face_x_p,MPI_face_y_p,1,1);
 
-    
-    //std::cout << rank << " " << lbx << " " << rbx << " " << 1+ lbx << " " << newDimX_x - 1 - rbx << " " << newDimX_x << std::endl;
     for (int i = 1 + lbx; i < newDimX_x - 1 - rbx; i++)
     {
         for (int j = 1 + lby; j < newDimY_x - 1 - rby; j++)
@@ -185,13 +168,6 @@ void IcoNS::solve_time_step(Real time)
                                                      50.0 / 120.0 * DT * functionF_u(Y2_x, Y2_y, Y2_z, i, j, k, time + 64.0 / 120.0 * DT) -
                                                      34.0 / 120.0 * DT * functionF_u(grid.u, grid.v, grid.w, i, j, k, time) -
                                                      16.0 / 120.0 * DT * (halo_phi[getHaloP(i+1 - resx,j,k)] - halo_phi[getHaloP(i - resx,j,k)]) / (DX);
-                //std::cout << rank << "??" << std::endl;
-                // if(rank==0)
-                // {
-                //     std::cout << getx(i,j ,k) << " is" << Y3_x[getx(i,j,k)] << " with " << Y2_x[getx(i,j,k)] << " "<<functionF_u(Y2_x, Y2_y, Y2_z, i, j, k, time + 64.0 / 120.0 * DT) << " "
-                //                                         << functionF_u(grid.u, grid.v, grid.w, i, j, k, time) << " " <<  (halo_phi[getHaloP(i+1,j,k)] - halo_phi[getHaloP(i,j,k)]) << std::endl;
-                //     int stop; std::cin >> stop;
-                // }
             }
         }
     }
@@ -263,10 +239,9 @@ void IcoNS::solve_time_step(Real time)
             }
         }
     }
-    //boundary.divergence(Y3_x, Y3_y, Y3_z, Y2_p, time + 80.0 / 120.0 * DT, 16.0);
-
+    
     poissonSolver->solveNeumannPoisson(Y2_p);
-
+    MPI_Barrier(cart_comm);
     // 3) y2_p exchange
     copyPressureToHalo(Y2_p,halo_p);
     MPI_Barrier(cart_comm);
@@ -401,7 +376,7 @@ void IcoNS::solve_time_step(Real time)
     //boundary.divergence(grid.u, grid.v, grid.w, Y2_p, time + DT, 40.0);
 
     poissonSolver->solveNeumannPoisson(Y2_p);
-
+    MPI_Barrier(cart_comm);
     // 5) y2_p exchange
     copyPressureToHalo(Y2_p,halo_p);
     MPI_Barrier(cart_comm);
@@ -452,69 +427,40 @@ void IcoNS::solve_time_step(Real time)
     }
 }
 
-//TODO: maybe change everything with the indexing function
 Real IcoNS::functionF_u(const std::vector<Real> &u, const std::vector<Real> &v, const std::vector<Real> &w, int i, int j, int k, Real t)
 {
-    int lu = i * newDimY_x * dim_z + j * dim_z + k;
-    int lv = i * newDimY_y * dim_z + j * dim_z + k;
-    int lw = i * newDimY_z * dim_z_z + j * dim_z_z + k;
-
-    Real value =-(u[lu] * (u[lu + newDimY_x * dim_z] - u[lu - newDimY_x * dim_z]) / (2.0 * DX) +
-             (v[lv] + v[lv + newDimY_y * dim_z] + v[lv - dim_z] + v[lv + newDimY_y * dim_z - dim_z]) / 4.0 * (u[lu + dim_z] - u[lu - dim_z]) / (2.0 * DY) +
-             (w[lw] + w[lw + newDimY_z * dim_z_z] + w[lw - 1] + w[lw + newDimY_z * dim_z_z - 1]) / 4.0 * (u[lu + 1] - u[lu - 1]) / (2.0 * DZ)) +
-           1 / RE * ((u[lu + newDimY_x * dim_z] - 2 * u[lu] + u[lu - newDimY_x * dim_z]) / (DX * DX) + (u[lu + dim_z] - 2 * u[lu] + u[lu - dim_z]) / (DY * DY) + (u[lu + 1] - 2 * u[lu] + u[lu - 1]) / (DZ * DZ));
+    Real value = -(u[getx(i, j, k)] * (u[getx(i + 1, j, k)] - u[getx(i - 1, j, k)]) / (2.0 * DX) +
+             (v[gety(i - resx, j + resy, k)] + v[gety(i + 1 - resx, j + resy, k)] + v[gety(i - resx, j - 1 + resy, k)] + v[gety(i + 1 - resx, j - 1 + resy, k)]) / 4.0 * (u[getx(i, j + 1, k)] - u[getx(i, j - 1, k)]) / (2.0 * DY) +
+             (w[getz(i - resx, j - resy, k)] + w[getz(i + 1 - resx, j - resy, k)] + w[getz(i - resx, j - resy, k - 1)] + w[getz(i + 1 - resx, j - resy, k - 1)]) / 4.0 * (u[getx(i, j, k + 1)] - u[getx(i, j, k - 1)]) / (2.0 * DZ)) +
+           1 / RE * ((u[getx(i + 1, j, k)] - 2 * u[getx(i, j, k)] + u[getx(i - 1, j, k)]) / (DX * DX) + (u[getx(i, j + 1, k)] - 2 * u[getx(i, j, k)] + u[getx(i, j - 1, k)]) / (DY * DY) + (u[getx(i, j, k + 1)] - 2 * u[getx(i, j, k)] + u[getx(i, j, k - 1)]) / (DZ * DZ));
 
     if(testCase==0){
         value += functionG_u(i-1 + offset_x_x, j-1 + offset_y_x, k, t);
     }
     return value;
-    // return -(u[lu] * (u[lu + newDimY_x * dim_z] - u[lu - newDimY_x * dim_z]) / (2.0 * DX) +
-    //          (v[lv] + v[lv + newDimY_y * dim_z] + v[lv - dim_z] + v[lv + newDimY_y * dim_z - dim_z]) / 4.0 * (u[lu + dim_z] - u[lu - dim_z]) / (2.0 * DY) +
-    //          (w[lw] + w[lw + newDimY_z * dim_z_z] + w[lw - 1] + w[lw + newDimY_z * dim_z_z - 1]) / 4.0 * (u[lu + 1] - u[lu - 1]) / (2.0 * DZ)) +
-    //        1 / RE * ((u[lu + newDimY_x * dim_z] - 2 * u[lu] + u[lu - newDimY_x * dim_z]) / (DX * DX) + (u[lu + dim_z] - 2 * u[lu] + u[lu - dim_z]) / (DY * DY) + (u[lu + 1] - 2 * u[lu] + u[lu - 1]) / (DZ * DZ)) +
-    //        functionG_u(i-1 + coords[0] * other_dim_x_x, j-1 + coords[1] * other_dim_y_x, k, t);
 }
 
 Real IcoNS::functionF_v(const std::vector<Real> &u, const std::vector<Real> &v, const std::vector<Real> &w, int i, int j, int k, Real t)
-{
-    int lu = i * newDimY_x * dim_z + j * dim_z + k;
-    int lv = i * newDimY_y * dim_z + j * dim_z + k;
-    int lw = i * newDimY_z * dim_z_z + j * dim_z_z + k;
+{   
+    Real value = -((u[getx(i + resx, j - resy, k)] + u[getx(i + resx, j - resy + 1, k)] + u[getx(i + resx - 1, j - resy, k)] + u[getx(i + resx - 1, j - resy + 1, k)]) / 4.0 * (v[gety(i + 1, j, k)] - v[gety(i - 1, j, k)]) / (2.0 * DX) +
+             v[gety(i, j, k)] * (v[gety(i, j + 1, k)] - v[gety(i, j - 1, k)]) / (2.0 * DY) +
+             (w[getz(i, j - resy, k)] + w[getz(i, j - resy + 1, k)] + w[getz(i, j - resy, k - 1)] + w[getz(i, j - resy + 1, k - 1)]) / 4.0 * (v[gety(i, j, k + 1)] - v[gety(i, j, k - 1)]) / (2.0 * DZ)) +
+           1 / RE * ((v[gety(i + 1, j, k)] - 2 * v[gety(i, j, k)] + v[gety(i - 1, j, k)]) / (DX * DX) + (v[gety(i, j + 1, k)] - 2 * v[gety(i, j, k)] + v[gety(i, j - 1, k)]) / (DY * DY) + (v[gety(i, j, k + 1)] - 2 * v[gety(i, j, k)] + v[gety(i, j, k - 1)]) / (DZ * DZ));
     
-    Real value = -((u[lu] + u[lu + dim_z] + u[lu - newDimY_x * dim_z] + u[lu - newDimY_x * dim_z + dim_z]) / 4.0 * ((v[lv + newDimY_y * dim_z] - v[lv - newDimY_y * dim_z]) / (2.0 * DX)) +
-             v[lv] * (v[lv + dim_z] - v[lv - dim_z]) / (2.0 * DY) +
-             (w[lw] + w[lw - 1] + w[lw + dim_z_z] + w[lw + dim_z_z - 1]) / 4.0 * (v[lv + 1] - v[lv - 1]) / (2.0 * DZ)) +
-           (1.0 / RE) * ((v[lv + newDimY_y * dim_z] - 2.0 * v[lv] + v[lv - newDimY_y * dim_z]) / (DX * DX) +
-                         (v[lv + dim_z] - 2.0 * v[lv] + v[lv - dim_z]) / (DY * DY) +
-                         (v[lv + 1] - 2.0 * v[lv] + v[lv - 1]) / (DZ * DZ));
     if(testCase==0){
         value += functionG_v(i-1 + offset_x_y, j-1 + offset_y_y, k, t);
     }
 
     return value;
-    // return -((u[lu] + u[lu + dim_z] + u[lu - newDimY_x * dim_z] + u[lu - newDimY_x * dim_z + dim_z]) / 4.0 * ((v[lv + newDimY_y * dim_z] - v[lv - newDimY_y * dim_z]) / (2.0 * DX)) +
-    //          v[lv] * (v[lv + dim_z] - v[lv - dim_z]) / (2.0 * DY) +
-    //          (w[lw] + w[lw - 1] + w[lw + dim_z_z] + w[lw + dim_z_z - 1]) / 4.0 * (v[lv + 1] - v[lv - 1]) / (2.0 * DZ)) +
-    //        (1.0 / RE) * ((v[lv + newDimY_y * dim_z] - 2.0 * v[lv] + v[lv - newDimY_y * dim_z]) / (DX * DX) +
-    //                      (v[lv + dim_z] - 2.0 * v[lv] + v[lv - dim_z]) / (DY * DY) +
-    //                      (v[lv + 1] - 2.0 * v[lv] + v[lv - 1]) / (DZ * DZ)) +
-    //        functionG_v(i-1 + coords[0] * other_dim_x_y, j-1 + coords[1] * other_dim_y_y, k, t);
 }
 
 Real IcoNS::functionF_w(const std::vector<Real> &u, const std::vector<Real> &v, const std::vector<Real> &w, int i, int j, int k, Real t)
 
 {
-    int lu = i * newDimY_x * dim_z + j * dim_z + k;
-    int lv = i * newDimY_y * dim_z + j * dim_z + k;
-    int lw = i * newDimY_z * dim_z_z + j * dim_z_z + k;
-
-    Real value = -((u[lu] + u[lu - newDimY_x * dim_z] + u[lu + 1] + u[lu - dim_z * newDimY_x + 1]) / 4.0 * (w[lw + newDimY_z * dim_z_z] - w[lw - newDimY_z * dim_z_z]) / (2.0 * DX) +
-                  (v[lv + 1] + v[lv - dim_z + 1] + v[lv] + v[lv - dim_z]) / 4.0 * (w[lw + dim_z_z] - w[lw - dim_z_z]) / (2.0 * DY) +
-                  w[lw] * (w[lw + 1] - w[lw - 1]) / (2.0 * DZ)) +
-                  (1.0 / RE) * ((w[lw + newDimY_z * dim_z_z] - 2.0 * w[lw] + w[lw - newDimY_z * dim_z_z]) / (DX * DX) +
-                         (w[lw + dim_z_z] - 2.0 * w[lw] + w[lw - dim_z_z]) / (DY * DY) +
-                         (w[lw + 1] - 2.0 * w[lw] + w[lw - 1]) / (DZ * DZ));
-    
+    Real value = -((u[getx(i + resx, j, k)] + u[getx(i + resx, j, k + 1)] + u[getx(i + resx - 1, j, k)] + u[getx(i + resx - 1, j, k + 1)]) / 4.0 * (w[getz(i + 1, j, k)] - w[getz(i - 1, j, k)]) / (2.0 * DX) +
+             (v[gety(i, j + resy, k)] + v[gety(i, j + resy - 1, k)] + v[gety(i, j + resy, k + 1)] + v[gety(i, j + resy - 1, k + 1)]) / 4.0 * (w[getz(i, j + 1, k)] - w[getz(i, j - 1, k)]) / (2.0 * DY) +
+             w[getz(i, j, k)] * (w[getz(i, j, k + 1)] - w[getz(i, j, k - 1)]) / (2.0 * DZ)) +
+           1 / RE * ((w[getz(i + 1, j, k)] - 2 * w[getz(i, j, k)] + w[getz(i - 1, j, k)]) / (DX * DX) + (w[getz(i, j + 1, k)] - 2 * w[getz(i, j, k)] + w[getz(i, j - 1, k)]) / (DY * DY) + (w[getz(i, j, k + 1)] - 2 * w[getz(i, j, k)] + w[getz(i, j, k - 1)]) / (DZ * DZ));
     if(testCase==0){
         value += functionG_w(i-1 + offset_x_z, j-1 + offset_y_z, k, t);
     }
