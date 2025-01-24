c0732fd58ae70671b3f9ac13b5592f42e4c03063
DarioSantoroDS <dario98.flash@gmail.com>
2025-01-24 17:46:12 +0000

    implemented Half Complex

--- src/poissonSolver.cpp
+++ src/poissonSolver.cpp
@@ -24,8 +24,11 @@ void PoissonSolver::solveNeumannPoisson(double *F)
 
     c2d->transposeY2Z_MajorIndex(py, pz);
 
+// Version using the total complex and the helper
+
     // 3. Z-Direction Transforms
     neumann = fftw_plan_dft_r2c_1d(zSize[2], nullptr, nullptr, FFTW_ESTIMATE);
+
     for (int i = 0; i < zSize[1] * zSize[0]; i++)
     {
         fftw_execute_dft_r2c(neumann, &pz[i * zSize[2]], &helper[i * (zSize[2]/2+1)]);
@@ -53,16 +56,87 @@ void PoissonSolver::solveNeumannPoisson(double *F)
     {
         helper[0][0] = 0.0;
         helper[0][1] = 0.0;
-
     }
 
     // 1. Z-Direction Transforms
     neumann = fftw_plan_dft_c2r_1d(zSize[2], nullptr, nullptr, FFTW_ESTIMATE);
+
     for (int i = 0; i < zSize[1] * zSize[0]; i++)
     {
         fftw_execute_dft_c2r(neumann, &helper[i * (zSize[2] / 2 + 1)], &pz[i * zSize[2]]);
     }
     fftw_destroy_plan(neumann);
+
+// Version using the Half complex
+
+    // // 3. Z-Direction Transforms
+    // // neumann = fftw_plan_dft_r2c_1d(zSize[2], nullptr, nullptr, FFTW_ESTIMATE);
+    // neumann = fftw_plan_r2r_1d(zSize[2], nullptr, nullptr, FFTW_R2HC ,FFTW_ESTIMATE);
+
+    // for (int i = 0; i < zSize[1] * zSize[0]; i++)
+    // {
+    //     // fftw_execute_dft_r2c(neumann, &pz[i * zSize[2]], &helper[i * (zSize[2]/2+1)]);
+    //     fftw_execute_r2r(neumann, &pz[i * zSize[2]], &pz[i * zSize[2] ]);
+    // }
+    // fftw_destroy_plan(neumann);
+
+    // // Divide by the eigenvalues
+    // for (int k = 0; k < zSize[0]; k++)
+    // {
+    //     for (int j = 0; j < zSize[1]; j++)
+    //     {
+    //         // for (int i = 0; i < zSize[2]/2+1; i++)
+    //             // {
+    //             //     // helper[j * (zSize[1]) * (zSize[2]/2+1) + k * (zSize[2]/2+1) + i][0] /= (2 / (DX * DX) * (std::cos(2.0*i * M_PI / (c2d->nxGlobal - 1)) - 1) +
+    //             //     //                                                          2 / (DY * DY) * (std::cos((j + c2d->coord[0] * zSize[1]) * M_PI / (c2d->nyGlobal - 1)) - 1) +
+    //             //     //                                                          2 / (DZ * DZ) * (std::cos((k + c2d->coord[1] * zSize[0]) * M_PI / (c2d->nzGlobal - 1)) - 1));
+    //             //     // helper[j * (zSize[1]) * (zSize[2]/2+1) + k * (zSize[2]/2+1) + i][1] /= (2 / (DX * DX) * (std::cos(2.0*i * M_PI / (c2d->nxGlobal - 1)) - 1) +
+    //             //     //                                                                 2 / (DY * DY) * (std::cos((j + c2d->coord[0] * zSize[1]) * M_PI / (c2d->nyGlobal - 1)) - 1) +
+    //             //     //                                                                 2 / (DZ * DZ) * (std::cos((k + c2d->coord[1] * zSize[0]) * M_PI / (c2d->nzGlobal - 1)) - 1));
+    //             // }
+    //         pz[j * (zSize[1]) * (zSize[2]) + k * (zSize[2]) + 0] /= (2 / (DX * DX) * (std::cos(2.0 * 0 * M_PI / (c2d->nxGlobal - 1)) - 1) +
+    //                                                                  2 / (DY * DY) * (std::cos((j + c2d->coord[0] * zSize[1]) * M_PI / (c2d->nyGlobal - 1)) - 1) +
+    //                                                                  2 / (DZ * DZ) * (std::cos((k + c2d->coord[1] * zSize[0]) * M_PI / (c2d->nzGlobal - 1)) - 1));
+
+    //         for (int i = 1; i < zSize[2] / 2; i++)
+
+    //         {
+
+    //             pz[j * (zSize[1]) * (zSize[2]) + k * (zSize[2]) + i] /= (2 / (DX * DX) * (std::cos(2.0 * i * M_PI / (c2d->nxGlobal - 1)) - 1) +
+    //                                                                      2 / (DY * DY) * (std::cos((j + c2d->coord[0] * zSize[1]) * M_PI / (c2d->nyGlobal - 1)) - 1) +
+    //                                                                      2 / (DZ * DZ) * (std::cos((k + c2d->coord[1] * zSize[0]) * M_PI / (c2d->nzGlobal - 1)) - 1));
+    //         }
+    //         pz[j * (zSize[1]) * (zSize[2]) + k * (zSize[2]) + zSize[2] / 2] /= (2 / (DX * DX) * (std::cos(2.0 * zSize[2] / 2 * M_PI / (c2d->nxGlobal - 1)) - 1) +
+    //                                                                             2 / (DY * DY) * (std::cos((j + c2d->coord[0] * zSize[1]) * M_PI / (c2d->nyGlobal - 1)) - 1) +
+    //                                                                             2 / (DZ * DZ) * (std::cos((k + c2d->coord[1] * zSize[0]) * M_PI / (c2d->nzGlobal - 1)) - 1));
+
+    //         for (int i = zSize[2]/2+1; i < zSize[2]; i++)
+    //         {
+    //             pz[j * (zSize[1]) * (zSize[2]) + k * (zSize[2]) + (i)] /= (2 / (DX * DX) * (std::cos(2.0 * (zSize[2]-i) * M_PI / (c2d->nxGlobal - 1)) - 1) +
+    //                                                                                    2 / (DY * DY) * (std::cos((j + c2d->coord[0] * zSize[1]) * M_PI / (c2d->nyGlobal - 1)) - 1) +
+    //                                                                                    2 / (DZ * DZ) * (std::cos((k + c2d->coord[1] * zSize[0]) * M_PI / (c2d->nzGlobal - 1)) - 1));
+    //         }
+
+    //     }
+    // }
+
+    // if (c2d->nRank == 0)
+    // {
+    //     pz[0] = 0.0;
+    // }
+
+    // // 1. Z-Direction Transforms
+    // // neumann = fftw_plan_dft_c2r_1d(zSize[2], nullptr, nullptr, FFTW_ESTIMATE);
+    // neumann = fftw_plan_r2r_1d(zSize[2], nullptr, nullptr, FFTW_HC2R, FFTW_ESTIMATE);
+
+    // for (int i = 0; i < zSize[1] * zSize[0]; i++)
+    // {
+    //     // fftw_execute_dft_c2r(neumann, &helper[i * (zSize[2] / 2 + 1)], &pz[i * zSize[2]]);
+    //     fftw_execute_r2r(neumann, &pz[i * zSize[2]], &pz[i * zSize[2]]);
+    // }
+    // fftw_destroy_plan(neumann);
+
+// Remaining Poisson Solver
 
     c2d->transposeZ2Y_MajorIndex(pz, py);
 
